ae49b60e199132452ea920b4795ed095393a11a0
cadewiley <wilecr18@wfu.edu>
2020-03-31 16:31:35 -0400

    data added

---
+++ Data.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Cade Wiley on 3/31/20.
+//
+#include "Data.h"
+Data::Data() {
+
+}
+Data::Data(double d){
+    data=d;
+}
+bool Data::operator<=(const Data &rhs) {
+    if(this->data <= rhs.data)
+    {return true;}
+    else{
+        return false;
+    }
+}
+bool Data::operator>(const Data &rhs) {
+    if(this->data > rhs.data)
+    {return true;}
+    else{
+        return false;
+    }
+}
---
+++ Data.h
@@ -0,0 +1,21 @@
+//
+// Created by Cade Wiley on 3/31/20.
+//
+#include "InsertionSort.cpp"
+using namespace std;
+
+class Data {
+public:
+    Data();
+    Data(double d);
+    bool operator<=(const Data &rhs);
+    bool operator>(const Data &rhs);
+
+private:
+    double data;
+};
+
+#ifndef BINARYINSERTIONSORT_DATA_H
+#define BINARYINSERTIONSORT_DATA_H
+
+#endif //BINARYINSERTIONSORT_DATA_H
